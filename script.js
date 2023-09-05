997233fa26291c498c98b0f66c429e3e95bd5ac8
Rasheed Sanni <99488324+Rashedhigh@users.noreply.github.com>
2023-09-05 05:48:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  function project_1_github() {
+    window.open(
+      "https://github.com/Sinzupro/web3cave-phase2", "_blank"
+    );
+  }
+
+  function project_1_web() {
+    window.open(
+      "https://web3cave.xyz/", "_blank"
+    );
+  }
+
+
+  function linkedin() {
+    window.open(
+      "https://ng.linkedin.com/in/rasheed-sanni-7498b4284", "_blank"
+    );
+  }
+
+
+  function mygithub() {
+    window.open(
+      "https://github.com/Sinzupro", "_blank"
+    );
+  }
